c824e401d56b6b1efe49fc72bd65e93996053263
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-21 01:47:45 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,22 @@
 class StudentsController < ApplicationController
 
+  def new
+    @students = Student.all
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
